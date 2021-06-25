7227ddd4d6978dcabb3348f37083bcceccadd0d7
prof-kayos <admin@profkayos.com>
2021-06-25 01:30:50 -0500

    Initial commit of inital-script

---
+++ scripts/initial-setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install vim git unattended-upgrades ssh-import-id ssh
+
+sudo systemctl enable ssh.service
+
+ssh-import-id-gh prof-kayos
+
+sed -i 's/#PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config
+
+sudo systemctl restart sshd.service
+
+ip addr | grep 'inet '
+
+sudo apt install git lsb-release
+
+cd
+git clone --depth=1 https://github.com/RetroPie/RetroPie-Setup.git
+
+sudo raspi-config
