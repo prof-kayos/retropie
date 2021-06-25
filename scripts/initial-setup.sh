3faee5ca6613f57d73ce2780a1c6f1c2135dc9c0
prof-kayos <admin@profkayos.com>
2021-06-25 04:05:11 -0500

    removed raspi-config

--- scripts/initial-setup.sh
+++ scripts/initial-setup.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 sudo apt update && sudo apt upgrade -y && sudo apt install vim git unattended-upgrades lsb-release ssh-import-id ssh && sudo systemctl enable ssh.service && ssh-import-id-gh prof-kayos && sed -i 's/#PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config && sudo systemctl restart sshd.service && ip addr | grep 'inet ' && git clone --depth=1 https://github.com/RetroPie/RetroPie-Setup.git
-
-sudo raspi-config
